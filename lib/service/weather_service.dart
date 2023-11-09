99ff2274f2a8edee5c9a29eca6b4e75836f437bd
timjozi911 <145546553+timjozi911@users.noreply.github.com>
2023-11-09 20:13:37 +0200

    refactor: variable name to lower camel case

--- lib/service/weather_service.dart
+++ lib/service/weather_service.dart
@@ -6,14 +6,14 @@ import 'package:weer_man/models/weather_model.dart';
 import 'package:http/http.dart' as http;
 
 class WeatherService {
-  static const BASE_URL = "http://api.openweathermap.org/data/2.5/weather";
+  static const baseURL = "http://api.openweathermap.org/data/2.5/weather";
   final String apiKey;
 
   WeatherService(this.apiKey);
 
   Future<Weather> getWeather(String cityName) async {
     final response = await http
-        .get(Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=metric'));
+        .get(Uri.parse('$baseURL?q=$cityName&appid=$apiKey&units=metric'));
 
     if (response.statusCode == 200) {
       return Weather.fromJson(jsonDecode(response.body));
