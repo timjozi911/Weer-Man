f62770b552a205e16777d37400f7bb012fe8189d
timjozi911 <145546553+timjozi911@users.noreply.github.com>
2023-11-12 21:05:54 +0200

    feat: build weather screen functionality

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:weer_man/auth/secrets.dart';
+import 'package:weer_man/models/weather_model.dart';
+import 'package:weer_man/service/weather_service.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -8,8 +12,50 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+// api key
+  final _weatherService = WeatherService(apiKey);
+  Weather? _weather;
+
+// fetch weather
+  _fetchWeather() async {
+    // get the current city
+    String cityName = await _weatherService.getCurrentCity();
+
+    // get weather for city
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
+// weather animations
+
+// initial state
+  @override
+  void initState() {
+    super.initState();
+    _fetchWeather();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // city name
+          Text(_weather?.cityName ?? "loading city name"),
+          // temprature
+          Text('${_weather?.temperature.round()} °C')
+        ],
+      ),
+    );
   }
 }
