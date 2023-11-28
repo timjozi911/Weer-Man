bbb5fc354eb063e843d84e53855c998fc780a33a
timjozi911 <145546553+timjozi911@users.noreply.github.com>
2023-11-28 08:53:34 +0200

    refactor: project updates

--- android/app/src/main/kotlin/com/example/weer_man/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/weer_man/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.todo_list
+package com.example.weer_man
 
 import io.flutter.embedding.android.FlutterActivity
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+
 import 'package:weer_man/main.dart';
 
 void main() {
